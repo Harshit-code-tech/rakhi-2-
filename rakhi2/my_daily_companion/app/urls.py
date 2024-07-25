d0c08db650f5687e4f46cf913059222b6c5da803
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-25 23:30:39 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -1,17 +1,9 @@
-# app/urls.py
-from rest_framework.routers import DefaultRouter
-from .kivy_views import HabitViewSet
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import d_views
-router = DefaultRouter()
-router.register(r'habits', HabitViewSet)
+from . import views
 
 urlpatterns = [
-    path('', d_views.home, name='home'),
-    path('mood_entries/', d_views.mood_entries, name='mood_entries'),
-    path('daily_goals/', d_views.daily_goals, name='daily_goals'),
-    # Other URL patterns
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.home, name='home'),
+    path('mood_tracker/', views.mood_tracker, name='mood_tracker'),
+    path('habit_tracker/', views.habit_tracker, name='habit_tracker'),
+    path('reward/', views.reward, name='reward'),
 ]
