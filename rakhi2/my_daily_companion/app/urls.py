9225b3fb9a115789e776f18ff8dcc8532c978cec
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-27 00:56:27 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('mood_tracker/', views.mood_tracker, name='mood_tracker'),
     path('habit_tracker/', views.habit_tracker, name='habit_tracker'),
     path('reward/', views.reward, name='reward'),
+    path('mood_history/', views.mood_history, name='mood_history'),
+    path('chatbot_room/', views.chatbot_room, name='chatbot_room'),
+    path('emotion_detection_room/', views.emotion_detection_room, name='emotion_detection_room'),
+    # Add other URL patterns here
+
 ]
