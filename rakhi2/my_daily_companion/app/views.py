25304b31550f6a3d00854550e8f72b322cd66425
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-27 00:31:11 +0530

    Update views.py

--- rakhi2/my_daily_companion/app/views.py
+++ rakhi2/my_daily_companion/app/views.py
@@ -1,24 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Mood, Habit, Reward
 
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def mood_tracker(request):
     if request.method == 'POST':
         mood = request.POST.get('mood')
-        Mood.objects.create(user=request.user, mood=mood)
-    return render(request, 'mood_tracker.html')
+        if mood:
+            Mood.objects.create(user=request.user, mood=mood)
+            return redirect('mood_tracker')
+    moods = Mood.objects.filter(user=request.user)
+    return render(request, 'mood_tracker.html', {'moods': moods})
 
+@login_required
 def habit_tracker(request):
     if request.method == 'POST':
         habit = request.POST.get('habit')
-        Habit.objects.create(user=request.user, name=habit)
-    return render(request, 'habit_tracker.html')
+        if habit:
+            Habit.objects.create(user=request.user, name=habit)
+            return redirect('habit_tracker')
+    habits = Habit.objects.filter(user=request.user)
+    return render(request, 'habit_tracker.html', {'habits': habits})
 
+@login_required
 def reward(request):
     if request.method == 'POST':
         image = request.FILES.get('image')
-        Reward.objects.create(user=request.user, image=image)
-    return render(request, 'reward.html')
+        if image:
+            Reward.objects.create(user=request.user, image=image)
+            return redirect('reward')
+    rewards = Reward.objects.filter(user=request.user)
+    return render(request, 'reward.html', {'rewards': rewards})
