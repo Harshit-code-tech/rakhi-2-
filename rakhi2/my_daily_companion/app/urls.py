3c7a70aa5491531ab1f4588b6285922f5fa1926b
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-27 21:21:00 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -1,3 +1,4 @@
+# app/urls.py
 from django.urls import path
 from . import views
 
@@ -7,6 +8,9 @@ urlpatterns = [
     path('notes/', views.notes, name='notes'),  # Updated from habit_tracker to notes
     path('reward/', views.reward, name='reward'),
     path('mood_history/', views.mood_history, name='mood_history'),
+
+    path('settings/', views.settings, name='settings'),
+    path('reminder/', views.reminder, name='reminder'),
     path('chatbot_room/', views.chatbot_room, name='chatbot_room'),
     path('emotion_detection_room/', views.emotion_detection_room, name='emotion_detection_room'),
 ]
