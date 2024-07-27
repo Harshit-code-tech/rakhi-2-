f8c702254d64b57a6d414cac097b4fb58d574527
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-27 18:30:08 +0530

    Update models.py

--- rakhi2/my_daily_companion/app/models.py
+++ rakhi2/my_daily_companion/app/models.py
@@ -1,25 +1,22 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 class Mood(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mood = models.CharField(max_length=255)
+    level = models.IntegerField()
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.mood} on {self.date}"
 
-
-class Habit(models.Model):
+class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    completed = models.BooleanField(default=False)
-    date = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
 
     def __str__(self):
-        return f"{self.user.username} - {self.name} on {self.date}"
-
+        return self.title
 
 class Reward(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
