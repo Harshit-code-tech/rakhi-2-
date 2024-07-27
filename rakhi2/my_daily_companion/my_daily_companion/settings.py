41af2d6f47f75c7b4d3cf9173bc7be3962446cac
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-27 19:54:29 +0530

    Update settings.py

--- rakhi2/my_daily_companion/my_daily_companion/settings.py
+++ rakhi2/my_daily_companion/my_daily_companion/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'my_daily_companion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
