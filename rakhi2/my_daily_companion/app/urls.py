8b9911c7163e43bce3c1fde064e9663ee9f827c7
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-27 18:30:18 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('mood_tracker/', views.mood_tracker, name='mood_tracker'),
-    path('habit_tracker/', views.habit_tracker, name='habit_tracker'),
+    path('notes/', views.notes, name='notes'),  # Updated from habit_tracker to notes
     path('reward/', views.reward, name='reward'),
     path('mood_history/', views.mood_history, name='mood_history'),
     path('chatbot_room/', views.chatbot_room, name='chatbot_room'),
