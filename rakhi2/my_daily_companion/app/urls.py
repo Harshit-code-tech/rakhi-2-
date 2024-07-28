bcc94a547bf4eb6108803acc6b2fff109af3e142
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-28 20:42:59 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('notes/', views.notes, name='notes'),  # Updated from habit_tracker to notes
     path('reward/', views.reward, name='reward'),
     path('mood_history/', views.mood_history, name='mood_history'),
-
     path('settings/', views.settings, name='settings'),
     path('reminder/', views.reminder, name='reminder'),
     path('chatbot_room/', views.chatbot_room, name='chatbot_room'),
+    path('delete_reward/<int:reward_id>/', views.delete_reward, name='delete_reward'),
+    path('delete_reminder/<int:reminder_id>/', views.delete_reminder, name='delete_reminder'),
+    path('delete_note/<int:note_id>/', views.delete_note, name='delete_note'),
+    path('delete_mood/<int:mood_id>/', views.delete_mood, name='delete_mood'),
     path('emotion_detection_room/', views.emotion_detection_room, name='emotion_detection_room'),
 ]
