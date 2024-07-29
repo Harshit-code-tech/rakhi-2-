3a36c868197fec046219ca7f6c66bc7066d96269
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-29 13:16:23 +0530

    Update urls.py

--- rakhi2/my_daily_companion/my_daily_companion/urls.py
+++ rakhi2/my_daily_companion/my_daily_companion/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('login/', user_views.login_view, name='login'),
     path('logout/', user_views.logout_view, name='logout'),
     path('register/', user_views.register, name='register'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
