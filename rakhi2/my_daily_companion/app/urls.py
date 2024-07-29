6fb87bd1e649fa63040b7a6a276a0bac283b8f26
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-29 12:27:38 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -5,16 +5,17 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('mood_tracker/', views.mood_tracker, name='mood_tracker'),
-    #path('notes/', views.notes, name='notes'),  # Updated from habit_tracker to notes
     path('journal/', views.journal, name='journal'),
     path('reward/', views.reward, name='reward'),
     path('mood_history/', views.mood_history, name='mood_history'),
     path('settings/', views.settings, name='settings'),
     path('reminder/', views.reminder, name='reminder'),
+
+    path('notes/', views.notes, name='notes'),
     path('chatbot_room/', views.chatbot_room, name='chatbot_room'),
     path('delete_reward/<int:reward_id>/', views.delete_reward, name='delete_reward'),
     path('delete_reminder/<int:reminder_id>/', views.delete_reminder, name='delete_reminder'),
-    #path('delete_note/<int:note_id>/', views.delete_note, name='delete_note'),
+    path('delete_note/<int:id>/', views.delete_note, name='delete_note'),
     path('delete_mood/<int:mood_id>/', views.delete_mood, name='delete_mood'),
     path('delete_journal/<int:journal_id>/', views.delete_journal, name='delete_journal'),
     path('emotion_detection_room/', views.emotion_detection_room, name='emotion_detection_room'),
