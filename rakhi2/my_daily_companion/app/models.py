79bb2f22b5731abfb98d814d7d2055d02ec2ab08
Harshit Ghosh <harshitghosh6@gmail.com>
2024-07-31 00:36:16 +0530

    Update models.py

--- rakhi2/my_daily_companion/app/models.py
+++ rakhi2/my_daily_companion/app/models.py
@@ -11,6 +11,7 @@ class Mood(models.Model):
     def __str__(self):
         return f"{self.date} - {self.mood} - {self.level}"
 
+
 class Journal(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, default='')
