567bf7c77eeda31f3ff0b507d1efb7791584ba2c
Harshit Ghosh <harshitghosh6@gmail.com>
2024-08-03 00:05:49 +0530

    Update urls.py

--- rakhi2/my_daily_companion/app/urls.py
+++ rakhi2/my_daily_companion/app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='home'),
+    path('home/', views.home, name='home'),
     # path('signup/', views.signup, name='signup'),
     path('mood_tracker/', views.mood_tracker, name='mood_tracker'),
     path('journal/', views.journal, name='journal'),
