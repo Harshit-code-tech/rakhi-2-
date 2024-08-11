9e2d37c83d75de74f8bae35c7f7ef92f7f7222f8
Harshit Ghosh <harshitghosh6@gmail.com>
2024-08-11 19:57:59 +0530

    Update activities.js

--- rakhi2/my_daily_companion/app/static/js/activities.js
+++ rakhi2/my_daily_companion/app/static/js/activities.js
@@ -1,86 +1,76 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Safe parsing of JSON data with a fallback to an empty array
-    function safeParse(data) {
-        try {
-            return JSON.parse(data);
-        } catch (e) {
-            console.error('Error parsing JSON:', e);
+    function safeParse(scriptId) {
+        const dataElement = document.getElementById(scriptId);
+        if (dataElement) {
+            try {
+                console.log("Parsing data from script ID:", scriptId);
+                return JSON.parse(dataElement.textContent);
+            } catch (e) {
+                console.error('Error parsing JSON from script ID:', scriptId, e);
+                return [];
+            }
+        } else {
+            console.error('No script element found for ID:', scriptId);
             return [];
         }
     }
 
-    // Calendar Heatmap
-    var calendarHeatmapData = safeParse('{{ calendar_heatmap_data|escapejs }}');
-    var heatmapChart = echarts.init(document.getElementById('calendar-heatmap'));
-    var heatmapOption = {
-        tooltip: {
-            position: 'top',
-            formatter: function (params) {
-                return params.value[0] + ': ' + params.value[1];
-            }
-        },
-        visualMap: {
-            min: 0,
-            max: 35,
-            inRange: {
-                color: ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594']
+    /**
+     * Initialize a heatmap chart.
+     * @param {string} elementId - The ID of the DOM element where the chart will be rendered.
+     * @param {Array} data - The data to be used in the heatmap.
+     */
+    function initializeHeatmap(elementId, data) {
+        const chart = echarts.init(document.getElementById(elementId));
+        const option = {
+            tooltip: {
+                position: 'top',
+                formatter: function (params) {
+                    return params.value[0] + ': ' + params.value[1];
+                }
             },
-            top: '35%'
-        },
-        calendar: {
-            range: new Date().getFullYear(),
-            cellSize: [20, 'auto'],
-            orient: 'horizontal'
-        },
-        series: [{
-            type: 'heatmap',
-            coordinateSystem: 'calendar',
-            data: calendarHeatmapData.map(item => [item.date, item.activity_count])
-        }]
-    };
-    heatmapChart.setOption(heatmapOption);
+            visualMap: {
+                min: 0,
+                max: 35,
+                inRange: {
+                    color: ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594']
+                },
+                top: '35%'
+            },
+            calendar: {
+                range: new Date().getFullYear(),
+                cellSize: [20, 'auto'],
+                orient: 'horizontal'
+            },
+            series: [{
+                type: 'heatmap',
+                coordinateSystem: 'calendar',
+                data: data.map(item => [item.date, item.activity_count])
+            }]
+        };
+        chart.setOption(option);
+    }
 
-    // Mood Tracker Pie
-    var moodTrackerData = safeParse('{{ mood_tracker_data|escapejs }}');
-    var moodTrackerPie = echarts.init(document.getElementById('mood-tracker-pie'));
-    var moodTrackerOption = {
-        series: [{
-            type: 'pie',
-            data: moodTrackerData.map(item => ({ name: item.date, value: item.count })),
-        }]
-    };
-    moodTrackerPie.setOption(moodTrackerOption);
+    /**
+     * Initialize a pie chart.
+     * @param {string} elementId - The ID of the DOM element where the chart will be rendered.
+     * @param {Array} data - The data to be used in the pie chart.
+     */
+    function initializePieChart(elementId, data) {
+        const chart = echarts.init(document.getElementById(elementId));
+        const option = {
+            series: [{
+                type: 'pie',
+                data: data.map(item => ({ name: item.date, value: item.count })),
+            }]
+        };
+        chart.setOption(option);
+    }
 
-    // Journal Pie
-    var journalData = safeParse('{{ journal_data|escapejs }}');
-    var journalPie = echarts.init(document.getElementById('journal-pie'));
-    var journalOption = {
-        series: [{
-            type: 'pie',
-            data: journalData.map(item => ({ name: item.date, value: item.count })),
-        }]
-    };
-    journalPie.setOption(journalOption);
-
-    // Reward Pie
-    var rewardData = safeParse('{{ reward_data|escapejs }}');
-    var rewardPie = echarts.init(document.getElementById('reward-pie'));
-    var rewardOption = {
-        series: [{
-            type: 'pie',
-            data: rewardData.map(item => ({ name: item.date, value: item.count })),
-        }]
-    };
-    rewardPie.setOption(rewardOption);
-
-    // Note Pie
-    var noteData = safeParse('{{ note_data|escapejs }}');
-    var notePie = echarts.init(document.getElementById('note-pie'));
-    var noteOption = {
-        series: [{
-            type: 'pie',
-            data: noteData.map(item => ({ name: item.date, value: item.count })),
-        }]
-    };
-    notePie.setOption(noteOption);
+    // Initialize the charts with their respective data
+    initializeHeatmap('calendar-heatmap', safeParse('calendar-heatmap-data'));
+    initializePieChart('mood-tracker-pie', safeParse('mood-tracker-data'));
+    initializePieChart('journal-pie', safeParse('journal-data'));
+    initializePieChart('reward-pie', safeParse('reward-data'));
+    initializePieChart('note-pie', safeParse('note-data'));
 });
