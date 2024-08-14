2fc9a9ed7f973f7655e18017e1f16f063df65b68
Harshit Ghosh <harshitghosh6@gmail.com>
2024-08-15 00:37:05 +0530

    need

--- rakhi-2-/rakhi2/my_daily_companion/static/js/animations.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const animatedBox = document.querySelector('.animated-box');
-    animatedBox.addEventListener('mouseenter', () => {
-        animatedBox.style.animation = 'bounce 1s ease-in-out';
-    });
-    animatedBox.addEventListener('animationend', () => {
-        animatedBox.style.animation = '';
-    });
-});
--- rakhi-2-/rakhi_kivy/app/screens/settings_screen.py
+++
@@ -1,8 +0,0 @@
-# settings_screen.py
-from kivy.uix.screenmanager import Screen
-
-
-class SettingsScreen(Screen):
-    def __init__(self, **kwargs):
-        super(SettingsScreen, self).__init__(**kwargs)
-        # Additional initialization here
