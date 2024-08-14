211c16c27bc7fcc1272602289411d9ec54593a4f
Harshit Ghosh <harshitghosh6@gmail.com>
2024-08-15 00:40:34 +0530

    sadsa

--- rakhi_kivy/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .kivy_models import Habit
-
-class HabitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habit
-        fields = '__all__'
